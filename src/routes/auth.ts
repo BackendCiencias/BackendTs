77ad46e34d7ab7f6b640c5cdeb7a5bc9da79bfa0
BackendCiencias_Admin <backend@cienciasperu.edu.pe>
2023-02-07 20:22:55 -0500

    Deploy 01.2

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,9 @@ const router: Router = Router();
 
 import {signin, signup, profile} from '../controllers/auth.controller'
 
+router.get('/', (req,res) => {
+    res.send('Backend alive... at least for now')
+})
 router.post('/api/auth/secretary/signup', signup);
 router.post('/api/auth/secretary/signin', signin);
 
