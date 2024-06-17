062ebc265480715b251fa0bbd4fb5ca67d90942d
MJSoto123 <jsotobe@unsa.edu.pe>
2024-06-16 20:48:54 -0500

    xd

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,16 +1,16 @@
 import { getStudents, createStudent, getStudentsById, getStudentsByParamId, getStudentsByDNI, signinStudent, createBulkStudents, modifyStudentData, modifyStudentImage} from './../controllers/student.controller';
-import { verifyToken, isSecretary} from '../middlewares/jwt.handle';
+import { verifyToken} from '../middlewares/jwt.handle';
 import { Router } from 'express';
 const router: Router = Router();
 
-router.post('/api/student/signup', [verifyToken, isSecretary], createStudent)
-router.post('/api/student/signupbulk', [verifyToken, isSecretary], createBulkStudents)
+router.post('/api/student/signup', [verifyToken], createStudent)
+router.post('/api/student/signupbulk', [verifyToken], createBulkStudents)
 router.post('/api/student/signin', signinStudent)
-router.get('/api/student', [verifyToken, isSecretary], getStudents)
-router.get('/api/student/:student_id', [verifyToken, isSecretary], getStudentsByParamId)
-router.post('/api/student/id', [verifyToken, isSecretary], getStudentsById)
-router.post('/api/student/dni', [verifyToken, isSecretary], getStudentsByDNI)
-router.post('/api/student/modify', [verifyToken, isSecretary], modifyStudentData);
-router.post('/api/student/image', [verifyToken, isSecretary], modifyStudentImage);
+router.get('/api/student', [verifyToken], getStudents)
+router.get('/api/student/:student_id', [verifyToken], getStudentsByParamId)
+router.post('/api/student/id', [verifyToken], getStudentsById)
+router.post('/api/student/dni', [verifyToken], getStudentsByDNI)
+router.post('/api/student/modify', [verifyToken], modifyStudentData);
+router.post('/api/student/image', [verifyToken], modifyStudentImage);
 
 export default router;
