00c83991f646742e2f2009242be6144ed75ac2f0
MJSoto123 <jsotobe@unsa.edu.pe>
2024-07-08 15:13:28 -0500

    Get Attendacnce by Classroom Sorted

--- dist/controllers/attendance.controller.js
+++ dist/controllers/attendance.controller.js
@@ -22,10 +22,20 @@ const studentAttendance = ({ body }, res) => __awaiter(void 0, void 0, void 0, f
     }
 });
 exports.studentAttendance = studentAttendance;
+function compareStudents(student1, student2) {
+    const { surname1: s1, surname2: s2, name1: n1, name2: n2 } = student1.names;
+    const { surname1: s1Other, surname2: s2Other, name1: n1Other, name2: n2Other } = student2.names;
+    return (s1.localeCompare(s1Other) ||
+        s2.localeCompare(s2Other) ||
+        n1.localeCompare(n1Other) ||
+        n2.localeCompare(n2Other));
+}
 const attendanceByGradeAndSection = ({ body }, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { grade, section, collegue, month } = body;
         const responseStudents = yield (0, student_services_1.studentsByGradeAndSection)(grade, section, collegue);
+        console.log(responseStudents);
+        const sortedStudents = responseStudents.sort(compareStudents);
         const populatedAttendance = yield Promise.all(responseStudents.map((student) => __awaiter(void 0, void 0, void 0, function* () { return yield (0, attendance_services_1.studentPoputaleAttendance)(student, month); })));
         res.send(populatedAttendance);
     }
--- src/controllers/attendance.controller.ts
+++ src/controllers/attendance.controller.ts
@@ -2,6 +2,7 @@ import { Request , Response } from 'express';
 import { handleHttp } from '../utils/error.handle';
 import { markAttendance, studentPoputaleAttendance} from './../services/attendance.services';
 import { findStudentByDNI, studentsByGradeAndSection } from './../services/student.services';
+import { IStudent } from 'models/student.model';
 
 export const studentAttendance =  async({body}: Request, res: Response) => {
     try{
@@ -12,10 +13,26 @@ export const studentAttendance =  async({body}: Request, res: Response) => {
     }
 };
 
+function compareStudents(student1: IStudent, student2: IStudent) {
+    const { surname1: s1, surname2: s2, name1: n1, name2: n2 } = student1.names;
+    const { surname1: s1Other, surname2: s2Other, name1: n1Other, name2: n2Other } = student2.names;
+
+    return (
+        s1.localeCompare(s1Other) ||
+        s2.localeCompare(s2Other) ||
+        n1.localeCompare(n1Other) ||
+        n2.localeCompare(n2Other)
+    );
+}
+
+
 export const attendanceByGradeAndSection = async({body}:Request, res: Response) => {
     try{
         const {grade, section, collegue, month} = body;
         const responseStudents = await studentsByGradeAndSection(grade, section, collegue);
+        console.log(responseStudents)
+        const sortedStudents = responseStudents.sort(compareStudents);
+
         const populatedAttendance = await Promise.all(
             responseStudents.map(async (student) => await studentPoputaleAttendance(student, month))
         );
